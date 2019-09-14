ff403591df18149b8bd800d49c8818f3fd4caa00
Aaron <aaronjohn.roque.iics@ust.edu.ph>
2019-09-14 22:16:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/roqueaaron/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.roqueaaron
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
